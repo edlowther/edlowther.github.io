2ea4113e85eefe927621ca3d5210c6e6d5f950d1
edfordshire <ed.lowther@gmail.com>
2017-01-19 20:23:53 +0000

    Moved to geolocation with postcodes.io

--- mapTest/js/app.js
+++ mapTest/js/app.js
@@ -1,72 +1,93 @@
-(function() {
-	$(document).ready( function() {
+(function () {
+    $(document).ready(function () {
 
-		var map; 
-		var layer; 
-		var filter = "Marker = 'placemark_circle' OR Marker = 'large_blue'"; 
-		map_and_layer = initialize(map, layer, filter);
-		map = map_and_layer['map']; 
-		layer = map_and_layer['layer'];  
+        var map;
+        var layer;
+        var filter = "Marker = 'placemark_circle' OR Marker = 'large_blue'";
+        map_and_layer = initialize(map, layer, filter);
+        map = map_and_layer['map'];
+        layer = map_and_layer['layer'];
 
-		var postcodeLookup = function(postcodeToTest) {
-			if (postcodeToTest in postcodes) {
-				return postcodes[postcodeToTest];  
-			} else { 
-				if (postcodeToTest.length > 3) {
-					postcodePart = postcodeToTest.substring(0, 4); 
-					console.log(postcodePart); 
-					if (postcodePart in postcodes) {
-						return postcodes[postcodePart];
-					}
-				} if (postcodeToTest.length > 2) {
-					postcodePart = postcodeToTest.substring(0, 3); 
-					console.log(postcodePart); 
-					if (postcodePart in postcodes) {
-						return postcodes[postcodePart]; 
-					}
-				} if (postcodeToTest.length > 1) {
-					postcodePart = postcodeToTest.substring(0, 2); 
-					console.log(postcodePart); 
-					if (postcodePart in postcodes) {
-						return postcodes[postcodePart]; 
-					}
-				}
-			}
-		}; 
-
-		$('#postcode').keyup(function(event) {
-			if (event.keyCode === 13 || event.keyCode === 32) {
-				postcodeToTest = $.trim($('#postcode').val().toUpperCase()); 
-				var latlng = postcodeLookup(postcodeToTest); 
-				console.log(latlng); 
-                if (latlng) {
-                    $('.errorMessage').addClass('notDisplayed');
-                    map.panTo(new google.maps.LatLng(latlng[0], latlng[1])); 
-                    map.setZoom(14);
-                } else {
-                    $('.errorMessage').removeClass('notDisplayed');
+        var postcodeLookup = function (postcodeToTest) {
+            if (postcodeToTest in postcodes) {
+                return postcodes[postcodeToTest];
+            } else {
+                if (postcodeToTest.length > 3) {
+                    postcodePart = postcodeToTest.substring(0, 4);
+                    console.log(postcodePart);
+                    if (postcodePart in postcodes) {
+                        return postcodes[postcodePart];
+                    }
                 }
-			}
-		}); 
-
-		$('#filters').on('change', function() {
-			console.log("filters changed"); 
-			var markers = []; 
-			$('.nurseryRating').each(function(){ 
-				if (this.checked) {
-					markers.push("'" + this.value + "'"); 
-				}
-			});
-			console.log(markers); 
-			filter = "'Marker' IN (" + markers.join(',') + ")"; 
-			console.log(filter); 
-			layer.setOptions({
-            query: {
-              select: "col4",
-              from: "1NVngiWyLZULKjvNpyDamWAj35Y2SQyIUZt1b-UYM",
-              where: filter
+                if (postcodeToTest.length > 2) {
+                    postcodePart = postcodeToTest.substring(0, 3);
+                    console.log(postcodePart);
+                    if (postcodePart in postcodes) {
+                        return postcodes[postcodePart];
+                    }
+                }
+                if (postcodeToTest.length > 1) {
+                    postcodePart = postcodeToTest.substring(0, 2);
+                    console.log(postcodePart);
+                    if (postcodePart in postcodes) {
+                        return postcodes[postcodePart];
+                    }
+                }
             }
-          });
-		});  
-	})
-})(); 
+        };
+
+        $('#postcode').keyup(function (event) {
+            if (event.keyCode === 13 || event.keyCode === 32) {
+                postcodeToTest = $.trim($('#postcode').val().toUpperCase());
+                $.ajax({
+                    url: "http://api.postcodes.io/postcodes/" + postcodeToTest.replace(" ", ""),
+                    success: function (result) {
+                        console.log(result);
+                        console.log(result.status);
+                        if (result.status === 200 && result.hasOwnProperty('result') && result.result.hasOwnProperty('latitude')) {
+                            var latlng = [result.result.latitude, result.result.longitude];
+                            $('.errorMessage').addClass('notDisplayed');
+                            map.panTo(new google.maps.LatLng(latlng[0], latlng[1]));
+                            map.setZoom(14);
+                        } else {
+                            $('.errorMessage').removeClass('notDisplayed');
+                        }
+                    }, 
+                    error: function() {
+                        $('.errorMessage').removeClass('notDisplayed');
+                    }
+                });
+
+//                var latlng = postcodeLookup(postcodeToTest);
+//                console.log(latlng);
+//                if (latlng) {
+//                    $('.errorMessage').addClass('notDisplayed');
+//                    map.panTo(new google.maps.LatLng(latlng[0], latlng[1]));
+//                    map.setZoom(14);
+//                } else {
+//                    $('.errorMessage').removeClass('notDisplayed');
+//                }
+            }
+        });
+
+        $('#filters').on('change', function () {
+            console.log("filters changed");
+            var markers = [];
+            $('.nurseryRating').each(function () {
+                if (this.checked) {
+                    markers.push("'" + this.value + "'");
+                }
+            });
+            console.log(markers);
+            filter = "'Marker' IN (" + markers.join(',') + ")";
+            console.log(filter);
+            layer.setOptions({
+                query: {
+                    select: "col4",
+                    from: "1NVngiWyLZULKjvNpyDamWAj35Y2SQyIUZt1b-UYM",
+                    where: filter
+                }
+            });
+        });
+    })
+})();
